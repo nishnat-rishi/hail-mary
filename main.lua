cdcb37badb17019ca7d9db9b1e842fb5c3977f6f
nishnat-rishi <nishnat.rishi@gmail.com>
2021-06-04 15:28:12 +0530

    Component testing first commit!

--- main.lua
+++ main.lua
@@ -3,21 +3,25 @@
 function love.load()
   component_tree = { -- root element
     d_props = {
+      x = 100, 100,
       width = 100, height = 50,
     },
     children = {
       { -- child #1
         d_props = {
+          x = 0, y = 0,
           width = 20, height = 20,
         }
       },
       { -- child #2
         d_props = {
+          x = 20, y = 0,
           width = 20, height = 20
         }
       },
       { -- child #3
         d_props = {
+          x = 40, y = 0,
           width = 20, height = 20
         }
       }
@@ -26,9 +30,9 @@ function love.load()
 end
 
 function love.update(dt)
-
+  
 end
 
 function love.draw()
-
+  -- love.graphics.rectangle()
 end
