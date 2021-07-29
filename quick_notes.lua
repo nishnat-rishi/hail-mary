0230d4e919d589ba8e08cc5c60b22b0ca3fb8c0a
nishnat.rishi <nishnat.rishi@gmail.com>
2021-07-29 08:28:14 +0530

    Added base borders (and an unused profiler (?)).

--- quick_notes.lua
+++ quick_notes.lua
@@ -1,14 +1,26 @@
 --[[
 
-  (done? spring.lua) Can encapsulae the various physicsy things into a
+  [X] (done? spring.lua) Can encapsulae the various physicsy things into a
   smol_physics.lua module with some cute ways to provide springyness
   and so on to components.
 
-  (DONE) Scroll stuff can be tested as well
+  [X] (DONE) Scroll stuff can be tested as well.
 
-  (they finally got it, yay! :") components need their own module!
+  [X] (they finally got it, yay! :") components need their own module!
   lots of tree_draw, attach_*, and so on and so forth lying around
   here and there!
+
+  [ ] event handling happens in a messy (if elsy) way, we can handle
+  them better in a O(1) beautiful ram accessing pattern, 
+  (handler.handlers[event]()) or something.
+
+  [ ] BIG CHANGE (but maybe really worth it), is to add a separate 
+  bordering mechanism / system. Stuff like effective_width and 
+  effective_height would have to be introduced? height and width might
+  be convertible to dimension as a vector, so that effective_dimension 
+  (or effective_dim) might be sensible. and it would eliminate lots of
+  code (at least at first glance on the side of the user, maybe later
+  on on the side of the code as well (?)).
 
   ----
 
